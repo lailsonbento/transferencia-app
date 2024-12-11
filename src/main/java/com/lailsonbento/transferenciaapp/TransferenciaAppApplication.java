ef641cacf69aeaeeeba67d419595d86b65e5a110
Lailson Bento <bentolailson.dev@gmail.com>
2024-12-11 07:27:17 -0300

    feat: add spring application

---
+++ src/main/java/com/lailsonbento/transferenciaapp/TransferenciaAppApplication.java
@@ -0,0 +1,13 @@
+package com.lailsonbento.transferenciaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferenciaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransferenciaAppApplication.class, args);
+    }
+
+}
